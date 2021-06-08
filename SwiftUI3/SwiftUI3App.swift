8d5e48df632793ab74c459248254befcb20e16f3
Jakub Charvat <jakcharvat@gmail.com>
2021-06-08 22:19:14 +0300

    Initial Commit

---
+++ SwiftUI3/SwiftUI3App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI3App.swift
+//  SwiftUI3
+//
+//  Created by Jakub Charvat on 08.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
